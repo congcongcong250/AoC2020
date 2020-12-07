20b9fab954f54c76ffd42796f81383e3b0607944
leewang <lirenxn@gmail.com>
2020-12-08 00:43:17 +1100

    Day4 part1, attempt with ugly global variable

---
+++ day4/part1.scm
@@ -0,0 +1,36 @@
+(load "../fileIO.scm")
+(load "../util.scm")
+
+(define lines (read-lines "input.txt"))
+(define passports '())
+(define cur (make-equal-hash-table))
+(define size 0)
+(define valid-count 0)
+
+(for-each
+  (lambda (line)
+    (if (not (eq? 0 (string-length line)))
+        (for-each 
+          (lambda (pair) 
+            (hash-table/put! cur (first pair) (second pair))) 
+          (map
+            (string-splitter 'delimiter #\:)
+            ((string-splitter) line)))
+        (begin 
+          (set! size (hash-table-size cur))
+          (if (or (eq? size 8) (and (null? (hash-table/get cur "cid" '())) (eq? size 7)))
+            (set! valid-count (+ valid-count 1)))
+          (set! passports (cons cur passports))
+          (set! cur (make-equal-hash-table)))))
+lines)
+
+(set! size (hash-table-size cur))
+(if (or (eq? size 8) (and (null? (hash-table/get cur "cid" '())) (eq? size 7)))
+  (set! valid-count (+ valid-count 1)))
+(set! passports (cons cur passports))
+    
+(define res valid-count)
+
+(if (null? res)
+  (print "No result")
+  (print "Result is " res))
